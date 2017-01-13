127a0d614134e599ed2f966130a6709280fdaed1
aprokashik <aprokashik@users.noreply.github.com>
2017-01-13 14:39:37 +0200

    Update presetup-ubuntu.sh

--- scripts/presetup-ubuntu.sh
+++ scripts/presetup-ubuntu.sh
@@ -22,7 +22,7 @@ if [[ "$B2D_INSTALL_MODE" == "full" ]] || [[ "$B2D_INSTALL_MODE" == "vm" ]] ; th
     INSTALLED="$(which virtualbox | grep -c virtualbox)"
     sudo apt-get update
     if [ ${INSTALLED} -eq 0 ]; then
-        echo "Installing virtualbox"
+        echo-green "Installing virtualbox"
         sudo apt-get install -y \
             virtualbox \
             virtualbox-dkms \
@@ -32,12 +32,12 @@ if [[ "$B2D_INSTALL_MODE" == "full" ]] || [[ "$B2D_INSTALL_MODE" == "vm" ]] ; th
     INSTALLED="$(which vagrant | grep -c vagrant)"
 
     if [ ${INSTALLED} -eq 0 ]; then
-        echo "Installing vagrant"
+        echo-green "Installing vagrant"
         sudo apt-get install -y vagrant
     fi
 
 
-    echo "Installing vagrant plugins"
+    echo-green "Installing vagrant plugins"
     for PLUGIN in "vagrant-vbguest" "vagrant-vbox-snapshot"; do
         vagrant plugin install "${PLUGIN}"
     done
@@ -46,31 +46,37 @@ fi
 
 # Docker and other dependencies
 if [[ "$B2D_INSTALL_MODE" == "full" ]] || [[ "$B2D_INSTALL_MODE" == "docker" ]] ; then
-	# Remove old docker version
-	sudo rm -f /usr/local/bin/docker >/dev/null 2>&1 || true
-	# Install docker
-	echo-green "Installing docker cli v${DOCKER_VERSION}..."
+    # Remove old docker version
+    sudo rm -f /usr/local/bin/docker >/dev/null 2>&1 || true
+    # Install docker
+    echo-green "Installing docker cli v${DOCKER_VERSION}..."
     
-    echo "[docker] Installing tools for apt repository management"
+    echo-green "[docker] Installing tools for apt repository management"
     sudo apt-get update
     sudo apt-get install apt-transport-https ca-certificates
 
-    echo "[docker] Add net GPG key"
+    echo-green "[docker] Add net GPG key"
     sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-    echo "[docker] Add docker apt repository"
+    echo-green "[docker] Add docker apt repository"
     echo "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -cs) main" | tee /etc/apt/sources.list.d/docker.list
 
-    echo "[docker] Installing docker !"
-    sudo apt-get update
-    sudo apt-get install docker-engine
-    sudo service docker start
+    # Remove old docker version
+    sudo rm -f /usr/local/bin/docker >/dev/null 2>&1 || true
+    # Install docker
+    echo-green "Installing docker cli v${DOCKER_VERSION}..."
+    curl -sSL -O "https://get.docker.com/builds/$(uname -s)/$(uname -m)/docker-$DOCKER_VERSION.tgz"
+    tar zxf docker-$DOCKER_VERSION.tgz
+    sudo mv docker/* /usr/local/bin
+    rm -rf docker-$DOCKER_VERSION*
 
+    # Remove old docker-compose version
+    sudo rm -f /usr/local/bin/docker-compose >/dev/null 2>&1 || true
+    # Install docker-compose
+    echo-green "Installing docker-compose v${DOCKER_COMPOSE_VERSION}..."
     echo "[docker] Installing docker-compose"
-    if [ ! -e /usr/local/bin/docker-compose ]; then
-        sudo curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
-    fi
+    sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 
