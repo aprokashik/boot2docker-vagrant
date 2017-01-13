114966e85d5fd4877cd1842304c98c70bf9cffe4
aprokashik <aprokashik@users.noreply.github.com>
2017-01-13 14:16:05 +0200

    Update presetup-ubuntu.sh

--- scripts/presetup-ubuntu.sh
+++ scripts/presetup-ubuntu.sh
@@ -44,3 +44,33 @@ if [[ "$B2D_INSTALL_MODE" == "full" ]] || [[ "$B2D_INSTALL_MODE" == "vm" ]] ; th
 fi
 
 
+# Docker and other dependencies
+if [[ "$B2D_INSTALL_MODE" == "full" ]] || [[ "$B2D_INSTALL_MODE" == "docker" ]] ; then
+	# Remove old docker version
+	sudo rm -f /usr/local/bin/docker >/dev/null 2>&1 || true
+	# Install docker
+	echo-green "Installing docker cli v${DOCKER_VERSION}..."
+    
+    echo "[docker] Installing tools for apt repository management"
+    sudo apt-get update
+    sudo apt-get install apt-transport-https ca-certificates
+
+    echo "[docker] Add net GPG key"
+    sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+    echo "[docker] Add docker apt repository"
+    echo "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -cs) main" | tee /etc/apt/sources.list.d/docker.list
+
+    echo "[docker] Installing docker !"
+    sudo apt-get update
+    sudo apt-get install docker-engine
+    sudo service docker start
+
+    echo "[docker] Installing docker-compose"
+    if [ ! -e /usr/local/bin/docker-compose ]; then
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+    fi
+fi
+
+
